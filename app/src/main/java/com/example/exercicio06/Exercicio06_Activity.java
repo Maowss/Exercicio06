92eaba8893fe9fef59cb1a96e07c05b6c4c3018a
Maowss <Maowss@gmail.com>
2019-05-29 14:17:07 -0300

    Todas as telas do exercicio 6 estão organizadas + Falta colocar as cores e ToolBar + Vincular as Paginas entre si

---
+++ app/src/main/java/com/example/exercicio06/Exercicio06_Activity.java
@@ -0,0 +1,13 @@
+package com.example.exercicio06;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercicio06_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercicio06_layout);
+    }
+}
